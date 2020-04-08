85e519ca715b2a810425466f1e2fe409fca6516f
BoringMind <super.d3280@ya.ru>
2020-04-08 10:51:16 +0300

    Refactorings. Moved integration tests to separate class. Moved helper methods, that are used by integrate and unit tests to separate class.

--- editor/editor/tests.py
+++ editor/editor/tests.py
@@ -1,7 +1,7 @@
 import os
 import json
 import re
-from typing import BinaryIO, Tuple
+from typing import BinaryIO
 import unittest
 from unittest.mock import patch
 
@@ -11,12 +11,33 @@ from django.test import TestCase
 from editor.editor.image_process import ImageProcess
 
 
-class ImageUploadTest(TestCase):
+class TestHelperMethods():
+    """Contains all the methods, needed in both integrate and unit tests."""
+
+    @classmethod
+    def get_test_image_path(cls, img_name: str) -> str:
+        """Get path to the image in the test folder."""
+        img_folder = os.path.join(settings.MEDIA_ROOT, 'test_img')
+        return os.path.join(img_folder, img_name)
+
+
+class IntegrationTests(TestCase):
+    # -----------------------------------------
+    # HELPER METHODS
+
+    # -----------------------------------------
+    # Filesystem operations
+    # -----------------------------------------
+
     def get_test_image_path(self, img_name: str) -> str:
         """Get path to the image in the test folder."""
         img_folder = os.path.join(settings.MEDIA_ROOT, 'test_img')
         return os.path.join(img_folder, img_name)
     
+    # -----------------------------------------
+    # Post images
+    # -----------------------------------------
+
     def post_image(self, image: BinaryIO) -> dict:
         response = self.client.post('/upload/', {
             'upload': image
@@ -30,13 +51,19 @@ class ImageUploadTest(TestCase):
         dict object with json response content.
         """
         path = self.get_test_image_path(image_name)
-
         with open(path, 'rb') as image:
             response = self.post_image(image)
-
         return response
 
-    def check_response_url(self, response_url: str):
+    # -----------------------------------------
+    # Working with responses
+    # -----------------------------------------
+
+    def get_error_from_response(self, response) -> str:
+        """Get an error message from received jsonreponse."""
+        return response['error']['message']
+
+    def check_received_url(self, response_url: str):
         """If the url in the response is not correct, raise an AssertError.
 
         Filename must be something like this: 7gXL4258iz7ePBG.jpg
@@ -51,18 +78,12 @@ class ImageUploadTest(TestCase):
             f"Received url: {response_url}"
         )
 
+    # -----------------------------------------
+    # TESTS
+
     def test_return_error_on_get_request(self):
         response = self.client.get('/upload/')
         self.assertEqual(response.status_code, 404)
-
-    def test_success_response_doesnt_contain_err_message(self):
-        img_path = self.get_test_image_path('image.jpg')
-
-        with open(img_path, 'rb') as image:
-            response = self.post_image(image)
-            
-        # There cannot be an error message in a successful request
-        self.assertTrue('error' not in response)
 
     @unittest.expectedFailure
     def test_failed_response_doesnt_contain_img_url(self):
@@ -82,10 +103,7 @@ class ImageUploadTest(TestCase):
             response = self.post_image(image)
         response_url = response['url']
 
-        self.check_response_url(response_url)
-
-    def get_error_from_response(self, response) -> str:
-        return response['error']['message']
+        self.check_received_url(response_url)
 
     @unittest.expectedFailure
     def test_return_correct_error_on_failed_image_upload(self):
@@ -101,6 +119,15 @@ class ImageUploadTest(TestCase):
         )
         self.assertEqual(ref_error, error)
 
+    def test_success_response_doesnt_contain_err_message(self):
+        img_path = self.get_test_image_path('image.jpg')
+        with open(img_path, 'rb') as image:
+            response = self.post_image(image)
+        # There cannot be an error message in a successful request
+        self.assertTrue('error' not in response)
+
+
+class ImageUploadTest(TestCase):
     @classmethod
     def split_filename(cls, filename: str):
         filename, ext = os.path.splitext(filename)
@@ -132,7 +159,7 @@ class ImageUploadTest(TestCase):
         """
         return ImageUploadTest.split_filename(filename)[1]
 
-    def filename_is_correct(self, filename: str):
+    def case_filename_check(self, filename: str):
         """Raise AssertException if the filename is not correct.
         
         Example of the filename param:
@@ -150,10 +177,7 @@ class ImageUploadTest(TestCase):
     def make_img_path(self, img_name: str) -> str:
         return os.path.join(settings.MEDIA_ROOT, 'test_img', img_name)
 
-    def get_error_message_from_response(self, response: dict) -> str:
-        return response['error']['message']
-
-    def image_checking_case(self, expected_result: str, img_name: str):
+    def case_check_image(self, expected_result: str, img_name: str):
         # img_path = self.make_img_path(img_name)
 
         path = self.make_img_path(img_name)
@@ -168,24 +192,24 @@ class ImageUploadTest(TestCase):
 
         self.assertEqual(expected_result, actual_result)
 
-    def image_checking_not_exists_case(self, img_name: str):
+    def case_image_not_exists(self, img_name: str):
         img_path = self.make_img_path(img_name)
         message = (f'Unable to open image: \'{img_path}\': '
                    'No such file or directory')
-        self.image_checking_case(message, img_name)
+        self.case_check_image(message, img_name)
 
 
-    def test_filename_is_correct(self):
-        self.filename_is_correct('simple.jpg')
-        self.filename_is_correct('simple.jpeg')
-        self.filename_is_correct('simple.png')
-        self.filename_is_correct('simple.giff')
-        self.filename_is_correct('simple.webp')
-        self.filename_is_correct('simple.tiff')
-        self.filename_is_correct('.jpg')
-        self.filename_is_correct('.vsd..sd.v.sdv.jpg')
+    def test_filename_check(self):
+        self.case_filename_check('simple.jpg')
+        self.case_filename_check('simple.jpeg')
+        self.case_filename_check('simple.png')
+        self.case_filename_check('simple.giff')
+        self.case_filename_check('simple.webp')
+        self.case_filename_check('simple.tiff')
+        self.case_filename_check('.jpg')
+        self.case_filename_check('.vsd..sd.v.sdv.jpg')
 
-    def test_filepath_is_correct(self):
+    def test_filepath_check(self):
         img_name = 'image.jpg'
 
         # We are testing only path to the file
@@ -199,25 +223,25 @@ class ImageUploadTest(TestCase):
 
         self.assertEqual(tested_path, ref_path)
 
-    def test_image_checking(self):
-        self.image_checking_case('Image check completed', 'image.jpg')
-        self.image_checking_case('Image check completed', 'image.png')
-        self.image_checking_case('Image check completed', 'image.webp')
-        self.image_checking_case('Image check completed', 'image.tiff')
-        self.image_checking_case('Image check completed', 'image.gif')
+    def test_image_check(self):
+        self.case_check_image('Image check completed', 'image.jpg')
+        self.case_check_image('Image check completed', 'image.png')
+        self.case_check_image('Image check completed', 'image.webp')
+        self.case_check_image('Image check completed', 'image.tiff')
+        self.case_check_image('Image check completed', 'image.gif')
 
-        # self.image_checking_case(
+        # self.case_check_image(
         #     'File is too large. Maximum allowed size is 2.5Mb', 'large.jpg'
         # )
 
-        self.image_checking_case('Unsupported mime type', 'html_page.jpg')
-        self.image_checking_case('Unsupported mime type', 'malicious_js.png')
-        self.image_checking_case('Unsupported mime type', 'icon.svg')
+        self.case_check_image('Unsupported mime type', 'html_page.jpg')
+        self.case_check_image('Unsupported mime type', 'malicious_js.png')
+        self.case_check_image('Unsupported mime type', 'icon.svg')
 
-        self.image_checking_not_exists_case('false_name.none')
-        self.image_checking_not_exists_case('images.jpg')
-        self.image_checking_not_exists_case('image.jpg1')
-        self.image_checking_not_exists_case('image.jpg.jpg')
+        self.case_image_not_exists('false_name.none')
+        self.case_image_not_exists('images.jpg')
+        self.case_image_not_exists('image.jpg1')
+        self.case_image_not_exists('image.jpg.jpg')
 
     def test_image_url(self):
         """Test image url generation."""
