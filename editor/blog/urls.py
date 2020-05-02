a777b28fa1fa85a18b420a44e913081b6428b585
BoringMind <super.d3280@ya.ru>
2020-05-02 09:02:55 +0300

    Added url routing. Added 404 page.

--- editor/blog/urls.py
+++ editor/blog/urls.py
@@ -7,4 +7,8 @@ from editor.blog import views
 
 urlpatterns = [
     path('', views.home_view, name="blog-home"),
+    path('contact', views.contact_view, name="blog-contact"),
+    path('about', views.about_view, name="blog-about"),
+    path('post', views.post_view, name="blog-post"),
+    path('404', views.page_not_found_view, name="blog-404"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- editor/blog/views.py
+++ editor/blog/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'home-masonry.html')
+
+
+def page_not_found_view(request):
+    return render(request, 'page-404.html')
+
+
+def contact_view(request):
+    return render(request, 'page-contact.html')
+
+
+def about_view(request):
+    return render(request, 'page-about.html')
+
+
+def post_view(request):
+    return render(request, 'post-image.html')
